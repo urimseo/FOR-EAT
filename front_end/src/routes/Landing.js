5598cf7783ca13b6f01a26bdacbf3922cf865f85
김윤하 <beupthere@gmail.com>
2022-03-13 00:06:39 +0900

    [S06P22A103-18][FE] Feat: Landing Page 제작
    - slide 1 제작
    
    
    Former-commit-id: 7f3b8af690960e9053a3df82256fe8ea9f0e40ac

--- front_end/src/routes/Landing.js
+++ front_end/src/routes/Landing.js
@@ -1,18 +1,133 @@
 import React from "react";
+import { Outlet } from "react-router-dom";
 import styled from "styled-components";
+import Landing_1 from "assets/img/Landing_1.jpg";
 
 const Container = styled.div`
-  margin: 0 10vw;
+  display: flex
+`
+const BackgroundImgContainer = styled.div`
+  display: inline-flex;
 `;
 
+const Title = styled.p`
+  font-family : Philosopher;
+  font-size: 2.2rem;
+  position: absolute;
+  margin-left: auto;
+  margin-right: auto;
+  margin-top: 2rem;
+  left: 0.8rem;
+  right: 0;
+  text-align: center;
+  letter-spacing: -1%;
+`;
+
+const Img = styled.img`
+  width: 50vw;
+  display: flex;
+`;
+
+const TextContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  padding: 4rem;
+  .point { 
+    height: 2rem;
+    display: inline;
+    font-size: 1.2rem;
+    font-weight: 300;
+    margin-top: 17rem;
+  }
+  .hr__short {
+    border: 0;
+    width: 3rem;
+    align: left;
+    height: 0.1rem;
+    background: black;
+    margin: 0.2rem 0
+  }
+  .title {
+    font-family: Playfair Display;
+    font-size: 3.8rem;
+    margin: 2rem 0 1.5rem 0;
+  }
+  .content {
+    font-size: 1rem;
+    font-weight: 200;
+    margin: 0.8rem 0;
+  }
+`
+const Discover = styled.a`
+  color: black;
+  cursor: pointer;
+  text-decoration: none;
+  font-size: 1.2rem;
+  font-weight: 400;
+  margin: 3rem 0;
+`
 const Landing = () => {
   return (
     <>
-      <Container>
-          Landing
-      </Container>
+      <div>
+        <Container>
+          <Title>FOR:EAT</Title>
+          <BackgroundImgContainer>
+            <Img src={Landing_1}/>
+          </BackgroundImgContainer>
+          <TextContainer>
+            <div className="point">Recommends  Thousands of  Recipes</div>
+            <div className="hr__short" />
+            <div className="title">Personalized</div>
+            <div className="content">
+              FOR:EAT recommends thousands of international recipes based on your preferences.
+              <br />
+              Collects your choices by a survey and gives you the recipes that you are looking for.
+            </div>
+            <div className="content">
+              The more specific answers, the more exact result you will get.
+            </div>
+            <div className="content">
+              FOR:EAT recommends thousands of international recipes based on your preferences.
+              <br />
+              Collects your choices by a survey and gives you the recipes that you are looking for.
+            </div>
+            <Discover href="">DISCOVER</Discover>
+          </TextContainer>
+        </Container>
+      </div>
+      <Outlet />
     </>
   );
 };
 
 export default Landing;
+
+
+// const BackgroundImgContainer = styled.div`
+//   display: flex;
+//   width: 100%;
+// `;
+
+// const Img = styled.div`
+//   width: 100%;
+//   height: 100%;
+//   display: inline-block;
+// `
+
+// const Landing = () => {
+//   return (
+//     <>
+//         <h1>
+//           FOR:EAT
+//         </h1>
+//         <BackgroundImgContainer>
+//           <img src={Landing} alt=""/>
+//         </BackgroundImgContainer>
+//       <Outlet />
+
+//     </>
+//   );
+// };
+
+// export default Landing;
