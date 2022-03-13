5c120d5ebb919a953fd0beb0e785206bbda05adc
김윤하 <beupthere@gmail.com>
2022-03-13 09:47:00 +0900

    [S06P22A103-18][FE] Feat: Landing Page 제작
    - slide 2 제작
    
    
    Former-commit-id: 8a5b1aaa5cc704e9e8bc21f3a50998094cd664ef

--- front_end/src/routes/Landing.js
+++ front_end/src/routes/Landing.js
@@ -2,12 +2,14 @@ import React from "react";
 import { Outlet } from "react-router-dom";
 import styled from "styled-components";
 import Landing_1 from "assets/img/Landing_1.jpg";
+import Landing_2 from "assets/img/Landing_2.jpg";
 
 const Container = styled.div`
-  display: flex
+  display: inline-flex;
 `
 const BackgroundImgContainer = styled.div`
   display: inline-flex;
+  width: 50%
 `;
 
 const Title = styled.p`
@@ -17,54 +19,73 @@ const Title = styled.p`
   margin-left: auto;
   margin-right: auto;
   margin-top: 2rem;
-  left: 0.8rem;
+  left: 0;
   right: 0;
   text-align: center;
   letter-spacing: -1%;
 `;
 
 const Img = styled.img`
-  width: 50vw;
+  width: 100%;
   display: flex;
 `;
 
 const TextContainer = styled.div`
-  display: flex;
+  display: inline-flex;
   flex-direction: column;
-  padding: 4rem;
+  width:50%;
+  .text__wrapper {
+    padding: 26rem 4rem 4rem 4rem; 
+  }
   .point { 
     height: 2rem;
-    display: inline;
     font-size: 1.2rem;
     font-weight: 300;
-    margin-top: 17rem;
-  }
-  .hr__short {
-    border: 0;
-    width: 3rem;
-    align: left;
-    height: 0.1rem;
-    background: black;
-    margin: 0.2rem 0
   }
   .title {
     font-family: Playfair Display;
     font-size: 3.8rem;
     margin: 2rem 0 1.5rem 0;
   }
+  .title__2 {
+    font-family: Playfair Display;
+    font-size: 3.8rem;
+    margin: 1rem 0 1rem 0;
+  }
+  .hr__left {
+    border: 0;
+    width: 3rem;
+    height: 0.1rem;
+    background: black;
+    text-align: right;
+  }
+  .Hr_right {
+    border: 0;
+    width: 3rem;
+    height: 0.1rem;
+    background: black;
+    margin: 0 0 0 95.5%;
+  }
   .content {
     font-size: 1rem;
     font-weight: 200;
-    margin: 0.8rem 0;
+    margin: 2rem 0;
+  }
+  .content__wrapper {
+    padding-left: 12rem;
   }
 `
 const Discover = styled.a`
+  display: inline-block;
   color: black;
   cursor: pointer;
   text-decoration: none;
   font-size: 1.2rem;
   font-weight: 400;
-  margin: 3rem 0;
+  margin: 2rem 0;
+  .rignt {
+    padding-left 20rem;
+  }
 `
 const Landing = () => {
   return (
@@ -76,25 +97,57 @@ const Landing = () => {
             <Img src={Landing_1}/>
           </BackgroundImgContainer>
           <TextContainer>
-            <div className="point">Recommends  Thousands of  Recipes</div>
-            <div className="hr__short" />
-            <div className="title">Personalized</div>
-            <div className="content">
-              FOR:EAT recommends thousands of international recipes based on your preferences.
-              <br />
-              Collects your choices by a survey and gives you the recipes that you are looking for.
+            <div className="text__wrapper">
+              <div className="point">Recommends  Thousands of  Recipes</div>
+              <div className="hr__left"/>
+              <div className="title">Personalized</div>
+              <div className="content">
+                FOR:EAT recommends thousands of international recipes based on your preferences.
+                <br />
+                Collects your choices by a survey and gives you the recipes that you are looking for.
+              </div>
+              <div className="content">
+                The more specific answers, the more exact result you will get.
+              </div>
+              <div className="content">
+                FOR:EAT recommends thousands of international recipes based on your preferences.
+                <br />
+                Collects your choices by a survey and gives you the recipes that you are looking for.
+              </div>
+              <Discover href="">DISCOVER</Discover>
             </div>
-            <div className="content">
-              The more specific answers, the more exact result you will get.
-            </div>
-            <div className="content">
-              FOR:EAT recommends thousands of international recipes based on your preferences.
-              <br />
-              Collects your choices by a survey and gives you the recipes that you are looking for.
-            </div>
-            <Discover href="">DISCOVER</Discover>
           </TextContainer>
         </Container>
+        <Container>
+          <Title>FOR:EAT</Title>
+          <TextContainer>
+            <div className="text__wrapper">
+              <div className="point" style={{textAlign: "right"}}>Tags</div>
+              {/* <Hr_left /> */}
+              <div className="Hr_right" />
+              <div className="title__2" style={{paddingLeft: "3rem"}}>Dessert</div>
+              <div className="title__2" style={{paddingLeft: "6rem"}}>Christmas</div>
+              <div className="title__2" style={{paddingLeft: "9rem"}}>Vegetarian</div>
+              <div className="content__wrapper">
+                <div className="content">
+                  Find the best recipes for your special occasions.
+                  <br />
+                  FOR:EAT provides several purified tags that helps you easily find great recipes.
+                </div>
+                <div className="content">
+                  Enjoy your meal:)
+                </div>
+                <div style={{alignItems: "end"}}>
+                  <Discover href="" style={{marginLeft: "73%"}}>DISCOVER</Discover>
+                </div>
+              </div>
+            </div>
+          </TextContainer>
+          <BackgroundImgContainer>
+            <Img src={Landing_2}/>
+          </BackgroundImgContainer>
+        </Container>
+
       </div>
       <Outlet />
     </>
@@ -102,32 +155,3 @@ const Landing = () => {
 };
 
 export default Landing;
-
-
-// const BackgroundImgContainer = styled.div`
-//   display: flex;
-//   width: 100%;
-// `;
-
-// const Img = styled.div`
-//   width: 100%;
-//   height: 100%;
-//   display: inline-block;
-// `
-
-// const Landing = () => {
-//   return (
-//     <>
-//         <h1>
-//           FOR:EAT
-//         </h1>
-//         <BackgroundImgContainer>
-//           <img src={Landing} alt=""/>
-//         </BackgroundImgContainer>
-//       <Outlet />
-
-//     </>
-//   );
-// };
-
-// export default Landing;
