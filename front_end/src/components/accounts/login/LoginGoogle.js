e49fa4219dbec2b5e009cbdc393ed815f5293abd
han9506 <han9506@naver.com>
2022-03-14 17:23:53 +0900

    [S06P22A103-82][FE] Feat: Google 로그인	수정
    Former-commit-id: 5a1ee388041f72cca9101c722ff895ea69c0571a

--- front_end/src/components/accounts/login/LoginGoogle.js
+++ front_end/src/components/accounts/login/LoginGoogle.js
@@ -1,6 +1,8 @@
 import React from "react";
 import GoogleLogin from 'react-google-login';
 import styled from "styled-components";
+import { useSetRecoilState } from 'recoil';
+import { isLoginState } from '../../../atoms/atoms';
 import { useNavigate } from 'react-router-dom';
 import { googleLogin } from '../../../api/AuthApi';
 
@@ -14,23 +16,35 @@ const Container = styled.div`
 `;
 
 
-const Google = () => {
+const LoginGoogle = () => {
   const navigate = useNavigate();
+  const setIsLoginState = useSetRecoilState(isLoginState);
 
   const successGoogle = async (response) => {
+    // console.log(response)
     const data = {
       googleId: response.profileObj.googleId,
       imageUrl: response.profileObj.imageUrl,
       name: response.profileObj.name
     };
     const result = await googleLogin(data, response.tokenId);
-    console.log(result);
-    navigate("/recommend");
+
+    if (result.data.status === 200) {
+      try {
+        setIsLoginState(true);
+        navigate("/recommend");
+      }
+      catch {
+        window.location.reload();
+      }
+    } else {
+      // alert("로그인 정보를 확인해주세요.");
+    }
   }
   
   const failGoogle = (response) => {
     console.log(response);
-    alert("로그인 정보를 확인해주세요.")
+    // alert("로그인 정보를 확인해주세요.")
   }
   
   return (
@@ -47,4 +61,4 @@ const Google = () => {
   );
 };
 
-export default Google;
+export default LoginGoogle;
