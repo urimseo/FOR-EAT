0f24a1f9fa444680082d356799aa7276238e8174
서우림 <rain7633@naver.com>
2022-03-14 18:44:09 +0900

    [S06P22A103-80][BE] Feat: 구글 소셜 로그인
    Former-commit-id: 733b26ee8afee08a3cbabfe521374835d779cfb6

--- back_end/members/serializers.py
+++ back_end/members/serializers.py
@@ -1,8 +1,16 @@
 from rest_framework import serializers
 from django.contrib.auth import get_user_model
 from .models import Member
-class MemberSerializer(serializers.ModelSerializer):
+
+class KaKaoMemberSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = Member
         fields = ('nickname', 'profile_image_url', 'kakao_id')
+
+
+class GoogleMemberSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Member
+        fields = ('nickname', 'profile_image_url', 'google_id')
