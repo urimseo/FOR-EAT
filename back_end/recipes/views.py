217fa2ff90b2f9e57fd03a6adff029ab56af2766
aletsire <aletsire@gmail.com>
2022-03-19 15:16:59 +0900

    [S06P22A103-105][BE] Feat: 레시피 카테고리 별 리스트 API
    - settings.py: 인피니티 스크롤 대비 DEFAULT_PAGINATION_CLASS -> 한페이지에 24개씩으로 지정
    - url, view, serialzer 수정
    
    
    Former-commit-id: 3c01134f339a9c9edaa9d43ccb69780e5500135d

--- back_end/recipes/views.py
+++ back_end/recipes/views.py
@@ -1,3 +1,4 @@
+from contextlib import nullcontext
 from django.http import Http404
 from django.shortcuts import render
 from django.db.models import Q
@@ -6,41 +7,54 @@ from rest_framework.generics import ListAPIView
 from rest_framework.response import Response
 from rest_framework.pagination import LimitOffsetPagination
 
-from recipes.models import Recipe, Ingredient
+from recipes.models import Recipe, Ingredient, Category
 from recipes.serializers import RecipeSerializer, RecipeListSerializer
 import json
+import re
 
 # Create your views here.
 
-# class RecipeList(ListAPIView, LimitOffsetPagination):
-#     def get_queryset(self):
-#         word = self.request.GET['category']
-#         if word:
-#             object_list = Recipe.objects.filter(
-#                 Q(ingredient_raw__icontains=word)
-#                 # Q(ingredient_raw__icontains=word) |
-#                 # Q(ingredient_raw__icontains=word)
-#             )
-#             return object_list
+class RecipeList(ListAPIView, LimitOffsetPagination):
+    def get_queryset(self):
+        word = self.request.GET['category']
+        if word in ['Europe', 'Asia', 'America', 'Africa', 'Austrailia', 'Beef', 'Pork', 'Lamb', 'Poultry', 'Chicken', 'Seafood', 'Vegetable',
+        'Bread', 'Rice', 'Pasta', 'Dessert',]:
+            category = Category.objects.filter(category_name=word)
+            object_list = Recipe.objects.filter(categories__in=category)
+            return object_list
+        elif word in ['One', 'Two', 'Four', 'Party']:
+            if word == 'One':
+                object_list = Recipe.objects.filter(servings=1)
+            elif word == 'Two':
+                object_list =Recipe.objects.filter(servings=2)
+            elif word == 'Four':
+                object_list =Recipe.objects.filter(servings=4)
+            else:
+                object_list =Recipe.objects.filter(servings__gt=4)
+            return object_list
+        elif word in ['30min', '60min', '120min', '180min', '24hours']:
+            time = int(re.sub(r'[^0-9]', '', word))
+            if time == 30:
+                object_list = Recipe.objects.filter(total_time__lte=time)
+            elif time == 60:
+                object_list = Recipe.objects.filter(total_time__gt=30, total_time__lte=time)
+            elif time == 120:
+                object_list = Recipe.objects.filter(total_time__gt=60, total_time__lte=time)
+            elif time == 180:
+                object_list = Recipe.objects.filter(total_time__gt=120, total_time__lte=time)
+            elif time == 24:
+                time *= 60
+                object_list = Recipe.objects.filter(total_time__gt=180, total_time__lte=time)
+            return object_list
 
-
-#         # word = self.request.GET['category']
-#         # seq = Ingredient.objects.get(ingredient_name=word)
-
-#         # if word:
-#         #     object_list = Recipe.ingredients.filter(
-#         #         ingredient_seq = seq
-#         #     )
-#         #     print(object_list)
-#         #     return object_list
-
-#     def get(self, request, format=None):
+    def get(self, request, format=None):
         
-#         recipes = self.get_queryset()
-#         results = self.paginate_queryset(recipes)
-#         serializer = RecipeListSerializer(results, many = True)
-
-#         return Response(serializer.data)
+        recipes = self.get_queryset()
+        results = self.paginate_queryset(recipes)
+        serializer = RecipeListSerializer(results, many = True)
+        for i in serializer.data:
+            i['images'] = json.loads(i['images'])[0]
+        return Response(serializer.data)
 
 
 # class RecipeDetail(APIView):
