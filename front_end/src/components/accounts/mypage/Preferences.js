87db4074e024a0d5855c3427a0a41adfde564dda
runkey <csi9509@naver.com>
2022-03-23 01:18:37 +0900

    [S06P22A103-88][FE] Feat: Preferences 추가
    Former-commit-id: 2a5111b6982d725406f47d179045e4eca1eb23c7

--- front_end/src/components/accounts/mypage/Preferences.js
+++ front_end/src/components/accounts/mypage/Preferences.js
@@ -1,6 +1,8 @@
 import React from "react";
 import styled from "styled-components";
-import Card from "components/commons/Card"
+import Modal from "components/commons/Modal";
+import Plus from "assets/img/Plus.png"
+import Rosemary from "assets/img/Ingredient_rosemary.jpg"
 
 const Container = styled.div`
 `
@@ -11,20 +13,170 @@ const Title = styled.div`
   margin-top: 3rem;
 `
 
-const CardContainer = styled.div`
-display: flex;
-flex-flow: wrap;
+const SurveyContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+`
+const SubTheme = styled.div`
+  margin-top: 3rem;
+`
+
+const SubTitle = styled.div`
+  font-size: 25px;
+`
+
+const Sub = styled.div`
+  font-size: 15px;
+  color: #8C8B8B;
+  margin-top: 1rem;
+`
+
+const Item = styled.div`
+  display: flex;
+  flex-direction: row;
+`
+
+const ImageContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  margin: 4rem;
+`;
+
+const Image = styled.img`
+  border-radius: 4rem;
+  height: 8rem;
+  width: 8rem;
+  cursor: pointer;
+`;
+
+const ImageSub = styled.div`
+  font-size: 15px;
+  margin-top: 1rem;
+  font-weight: bold;
+
+`;
+
+const Diets = styled.div`
+`
+
+const Allergies = styled.div`
+`
+
+const DislikedIngredients = styled.div`
+`
+
+const FavoriteCuisines = styled.div`
+`
+
+const Goals = styled.div`
 `
 
 const Preferences = () => {
   return (
     <>
       <Container>
-        <Title>Preferences</Title>
+        <Title>My Preferences</Title>
 
-        <CardContainer>
-          <Card />
-        </CardContainer>
+        <SurveyContainer>
+
+          <Diets>
+            <SubTheme>
+              <SubTitle>Diets</SubTitle>
+              <Sub>Click ‘ADD DIETS’ and Select from the diets below.</Sub>
+            </SubTheme>
+
+            <Item>
+              <ImageContainer>
+                <Image src={Rosemary}/>
+                <ImageSub>VEGETARIAN</ImageSub>
+              </ImageContainer>
+                
+              <ImageContainer>
+                <Image src={Plus}/>
+                <ImageSub>ADD DIETS</ImageSub>
+              </ImageContainer>
+            </Item>
+          </Diets>
+
+          <Allergies>
+            <SubTheme>
+            <SubTitle>Allergies</SubTitle>
+              <Sub>Click ‘Allergies’ and Select from the allergies below.</Sub>
+            </SubTheme>
+
+            <Item>
+              <ImageContainer>
+                <Image src={Rosemary}/>
+                <ImageSub>VEGETARIAN</ImageSub>
+              </ImageContainer>
+                
+              <ImageContainer>
+                <Image src={Plus}/>
+                <ImageSub>ADD DIETS</ImageSub>
+              </ImageContainer>
+            </Item>
+          </Allergies>
+
+          <DislikedIngredients>
+            <SubTheme>
+              <SubTitle>Disliked Ingredients</SubTitle>
+              <Sub>Click ‘ADD DISLIKED INGREDIENTS’ and Add ingredient that you don’t like.</Sub>
+            </SubTheme>
+
+            <Item>
+              <ImageContainer>
+                <Image src={Rosemary}/>
+                <ImageSub>VEGETARIAN</ImageSub>
+              </ImageContainer>
+                
+              <ImageContainer>
+                <Image src={Plus}/>
+                <ImageSub>ADD DIETS</ImageSub>
+              </ImageContainer>
+            </Item>
+          </DislikedIngredients>
+
+          <FavoriteCuisines>
+            <SubTheme>
+              <SubTitle>Favorite Cuisines</SubTitle>
+              <Sub>Click ‘ADD FAVORITE CUISINES’ and Add Cuisine that you like.</Sub>
+            </SubTheme>
+
+            <Item>
+              <ImageContainer>
+                <Image src={Rosemary}/>
+                <ImageSub>VEGETARIAN</ImageSub>
+              </ImageContainer>
+                
+              <ImageContainer>
+                <Image src={Plus}/>
+                <ImageSub>ADD DIETS</ImageSub>
+              </ImageContainer>
+            </Item>
+          </FavoriteCuisines>
+
+          <Goals>
+            <SubTheme>
+              <SubTitle>Goals</SubTitle>
+              <Sub>Click ‘ADD GOALS’ and Select your goals.</Sub>
+            </SubTheme>
+
+            <Item>
+              <ImageContainer>
+                <Image src={Rosemary}/>
+                <ImageSub>VEGETARIAN</ImageSub>
+              </ImageContainer>
+                
+              <ImageContainer>
+                <Image src={Plus}/>
+                <ImageSub>ADD DIETS</ImageSub>
+              </ImageContainer>
+            </Item>
+          </Goals>
+
+        </SurveyContainer>
       </Container>
     </>
   );
