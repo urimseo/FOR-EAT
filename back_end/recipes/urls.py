45c2f6bf706b9daac9ae4fb8bf0858bb76b9e22a
aletsire <aletsire@gmail.com>
2022-03-23 13:51:15 +0900

    [S06P22A103-133][BE] Feat: 리뷰 삭제 API
    Former-commit-id: 56faedfca110fd3495461894e7dfcf97c88df9a0

--- back_end/recipes/urls.py
+++ back_end/recipes/urls.py
@@ -4,5 +4,6 @@ from . import views
 urlpatterns = [ 
     path('', views.RecipeList.as_view()),
     path('<int:pk>', views.RecipeDetail.as_view()),
-    path('<int:pk>/review/', views.Reviews.as_view()),
+    path('<int:pk>/review/', views.ReviewList.as_view()),
+    path('review/<int:id>', views.ReviewDetail.as_view()),
 ]
