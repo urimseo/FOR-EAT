1c55b70204500331d6622642639852515b96fc98
runkey <csi9509@naver.com>
2022-03-28 05:06:05 +0900

    [S06P22A103-139][FE] Feat: 리뷰 레시피 리스트 임시 추가
    Former-commit-id: 0ed8372566ff1f0b4f553f7ae2eb19037e0020d5

--- front_end/src/components/accounts/mypage/ReviewRecipeList.js
+++ front_end/src/components/accounts/mypage/ReviewRecipeList.js
@@ -1,17 +1,13 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import styled from "styled-components";
-// import img from "assets/img/Ingredient_rosemary.jpg";
-import img from "assets/img/Landing_1.jpg";
+
+import ReviewCard from "components/recipeDetail/ReviewCard"
+import ReviewForm from "components/recipeDetail/ReviewForm"
 
 const Container = styled.div`
-`
-
-const Top = styled.div`
   display: flex;
-  justify-content: space-between;
-  width: 100%;
-  margin-top: 3rem;
-  font-family: Playfair Display;
+  flex-direction: column;
+  justify-content: center;
 `
 
 const Title = styled.div`
@@ -20,90 +16,36 @@ const Title = styled.div`
   font-size: 64px;
 `
 
-const See = styled.a`
-  display: flex;
-  align-items: center;
-  font-size: 20px;
-  margin-left: auto;
-  text-decoration: none;
-`
 
-const Review = styled.div`
-    display: flex;
-    flex-direction: row;
-    margin-top: 3rem;
-`
 
-const Image = styled.img`
-  position: absolute;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  height: 14rem;
-  width: 14rem;
-  margin-left: 1rem;
-`
+const ReviewList = ({ReviewList}) => {
+  const [reviews, setReviews] = useState([]); 
+  console.log(ReviewList)
 
-const Text = styled.div`
-  background-color: #F2F2F2;
-  padding: 3rem;
-  padding-left: 5rem;
-  margin-top: 3rem;
-`
-
-const Sub = styled.div`
-  font-weight: bold;
-  font-size: 25px;
-`
-
-const Date = styled.div`
-  margin-top: 0.3rem;
-  font-size: 17px;
-  color: #999999;
-
-`
-
-const Rate = styled.div`
-margin-top: 0.3rem;
-  font-size: 22px;
-
-`
-
-const Contents = styled.div`
-margin-top: 1rem;
-  font-size: 18px;
-
-`
-
-const ReviewRecipeList = () => {
   return (
-    <>
       <Container>
-        <Top>
-          <Title>All Reviews</Title>
-          <See href="/mypage">See all →</See>
-        </Top>
-        <Review>
-          <Image src={img} alt="이미지를 찾을 수 없습니다." />
-          <Text>
-            <Sub>
-                PASTA WITH SAUSAGE, TOMATOES, AND CREAM
-            </Sub>
-            <Date>
-                2022.03.09
-            </Date>
-            <Rate>
-                ★★★★★
-            </Rate>
-            <Contents>
-                FOR:EAT recommends thousands of international recipes based on your preferences.
-                Collects your choices by a survey and gives you the recipes that you are looking for.
-            </Contents>
-          </Text>
-        </Review>
+        <Title>My Wish Recipes</Title>
+        { ReviewList.map((review) => ( 
+          <ReviewForm recipeId={review.recipe_seq} />
+        ))}
+        <div style={{display:"flex", justifyContent:"center", flexWrap:"wrap" }}>
+          { ReviewList.map((review) => ( 
+            <ReviewCard
+              key={review.id}
+              reviewId={review.id}
+              recipeId={review.recipe_seq}
+              memberName={review.member_nickname}
+              profileImgUrl={review.profile_image_url}
+              imgUrl={review.image_url}
+              content={review.content}
+              ratings={review.ratings}
+              lastModifiedDate={review.last_modified_date}
+            />
+          ))}
+        </div>
+        
       </Container>
-    </>
   );
 };
 
-export default ReviewRecipeList;
+export default ReviewList;
