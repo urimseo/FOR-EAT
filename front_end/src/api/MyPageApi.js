8d263c4991b35d9ddeb41e8d45069c6bf224f240
runkey <csi9509@naver.com>
2022-03-29 03:58:24 +0900

    [S06P22A103-183][FE] Feat: pagination 수정
    Former-commit-id: 4640caa5bbeaf9b937342625fb3f7cf40f3b5250

--- front_end/src/api/MyPageApi.js
+++ front_end/src/api/MyPageApi.js
@@ -1,52 +1,52 @@
-import { instance, fileInstance } from '../api/Axios';
+import { instance, fileInstance } from "../api/Axios";
 
 export const getMember = async (member_seq) => {
   const response = await instance.get(`/members/${member_seq}`);
   return response.data;
-}
+};
 
 export const editMember = async (member_seq, formData) => {
-    const response = await fileInstance.patch(`/members/${member_seq}`, formData);
-    return response.data;
-  }
+  const response = await fileInstance.patch(`/members/${member_seq}`, formData);
+  return response.data;
+};
 
 export const getMypage = async (member_seq) => {
-    const response = await instance.get(`/members/${member_seq}/mypage`);
-    return response.data;
-  }
+  const response = await instance.get(`/members/${member_seq}/mypage`);
+  return response.data;
+};
 
-  export const getWish = async (page, member_seq) => {
-    console.log(page)
-    const response = await instance.get(`/members/${member_seq}/mypage/likes`,{
-      params: {
-        limit: 2,
-        offset: 2*(page-1)
-      },
-    });
-    return response.data;
-  }
+export const getWish = async (page, member_seq) => {
+  console.log(page);
+  const response = await instance.get(`/members/${member_seq}/mypage/likes`, {
+    params: {
+      limit: 12,
+      offset: 12 * (page - 1),
+    },
+  });
+  return response.data;
+};
 
-  export const getReview = async (page, member_seq) => {
-    const response = await instance.get(`/members/${member_seq}/mypage/reviews`,{
-      params: {
-        limit: 24,
-        offset: 24*(page-1)
-      },
-    });
-    return response.data;
-  }
+export const getReview = async (page, member_seq) => {
+  const response = await instance.get(`/members/${member_seq}/mypage/reviews`, {
+    params: {
+      limit: 12,
+      offset: 12 * (page - 1),
+    },
+  });
+  return response.data;
+};
 
-  export const getSurvey = async (member_seq) => {
-    const response = await instance.get(`/members/${member_seq}/survey`);
-    return response.data;
-  }
+export const getSurvey = async (member_seq) => {
+  const response = await instance.get(`/members/${member_seq}/survey`);
+  return response.data;
+};
 
-  export const editSurvey = async (member_seq) => {
-    const response = await instance.patch(`/members/${member_seq}/survey`);
-    return response.data;
-  }
-  
-  export const createSurvey = async (member_seq) => {
-    const response = await fileInstance.post(`/members/${member_seq}/survey`);
-    return response.data;
-  }
+export const editSurvey = async (member_seq) => {
+  const response = await instance.patch(`/members/${member_seq}/survey`);
+  return response.data;
+};
+
+export const createSurvey = async (member_seq) => {
+  const response = await fileInstance.post(`/members/${member_seq}/survey`);
+  return response.data;
+};
