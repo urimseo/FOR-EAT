0f34fee219228c1d7199823f0398f76ea6918787
runkey <csi9509@naver.com>
2022-03-29 03:59:11 +0900

    [S06P22A103-182][FE] Design: 리뷰 카드 디자인 변경
    Former-commit-id: b22685f3c7f60026e1ce79d3bffb4f44d7b01584

--- front_end/src/components/accounts/mypage/ReviewCard.js
+++ front_end/src/components/accounts/mypage/ReviewCard.js
@@ -1,56 +1,45 @@
-import React, { useEffect } from "react";
+import React, { useState, useEffect } from "react";
+import { Link } from "react-router-dom";
 import styled from "styled-components";
-import Rating from '@mui/material/Rating';
+import Rating from "@mui/material/Rating";
 
 import { deleteReview } from "api/ReviewApi";
-import profileImg from "assets/img/Landing_1.jpg";
 import iconDelete from "assets/img/icon_delete.png";
+import { getRecipeDetail } from "api/RecipeDetailApi";
 
 const Container = styled.div`
   display: flex;
   width: 80%;
   height: 15rem;
   margin: 1rem;
-`
+`;
 
 const CardContainer = styled.div`
-  background-color: #F2F2F2;
+  background-color: #f2f2f2;
   padding: 2.5rem;
   display: flex;
-  width: 70%;
-`
+  width: 50%;
+`;
 
 const FlexContainer = styled.div`
   display: flex;
   flex-direction: row;
-`
+`;
 const SpaceBetweenContainer = styled.div`
   display: flex;
   justify-content: space-between;
-`
+`;
 const ImgWrapper = styled.div`
   width: 30%;
-  overflow: hidden; 
-  background-position: center;
-`
-
-const ProfileImgWrapper = styled.div`
-  width: 7rem;
   overflow: hidden;
   background-position: center;
-`
-
-const ProfileImg = styled.img`
-  border-radius: 4rem;
-  height: 5rem;
-  width: 5rem;
-`
+`;
 
 const TextContainer = styled.div`
   display: flex;
   flex-direction: column;
   width: 30rem;
-`
+`;
 
 const Name = styled.div`
   display: flex;
@@ -58,80 +47,88 @@ const Name = styled.div`
   font-size: 1.3rem;
   font-weight: 300;
   padding-left: 0.1rem;
-`
+`;
 
 const Date = styled.div`
   margin-top: 0.3rem;
   font-size: 1rem;
   color: #999999;
   padding: 1rem 0 1rem 0.1rem;
-`
+`;
 
 const Contents = styled.div`
   margin-top: 1rem;
   font-size: 18px;
-  color: #4A4A4A;
-  text-overflow:ellipsis;
+  color: #4a4a4a;
+  text-overflow: ellipsis;
   overflow: hidden;
   word-wrap: break-word;
   display: -webkit-box;
   -webkit-line-clamp: 2;
   -webkit-box-orient: vertical;
-`
+`;
 const Icon = styled.img`
   width: 1rem;
   height: 1rem;
   padding: 0 0.2rem;
   &:hover {
-    cursor : pointer;
+    cursor: pointer;
   }
-`
+`;
 
 const Img = styled.img`
   width: 100%;
   height: 100%;
-  object-fit: cover;
-`
+  object-fit: fill;
+`;
 
-const ReviewCard = ({ reviewId, memberName, imgUrl, profileImgUrl, content, ratings, lastModifiedDate }) => {
+const ReviewCard = ({
+  reviewId,
+  content,
+  ratings,
+  lastModifiedDate,
+  recipe_seq,
+}) => {
+  const [recipeImg, setRecipeImg] = useState([]);
+  const [recipeName, setRecipeName] = useState([]);
+  const [flag, setFlag] = useState(0);
 
   const onClickDelete = async () => {
     await deleteReview(reviewId);
-  }
-
+    setFlag(1);
+  };
+  useEffect(() => {
+    getRecipeDetail(recipe_seq).then((res) => {
+      setRecipeImg(res.images);
+      setRecipeName(res.name);
+      console.log("getRecipeDetail", res);
+    });
+  }, []);
   return (
-      <Container>
-        <CardContainer>
-          <FlexContainer>
-            <ProfileImgWrapper>
-              <ProfileImg src={profileImgUrl} alt="" />
-            </ProfileImgWrapper>
-            <TextContainer>
-              <SpaceBetweenContainer>
-                <Name>
-                  {memberName}
-                </Name>
-                {  }
-                <FlexContainer >
-                  <Icon src={iconDelete} onClick={onClickDelete}/>
-                </FlexContainer>
-              </SpaceBetweenContainer>
-              <Date>
-                {(lastModifiedDate).slice(0, 10)}
-              </Date>
-              <FlexContainer style={{justifyContent: "space-between"}}>
-                <Rating name="read-only" value={ratings} readOnly size="small" />
+    <Container>
+      <CardContainer>
+        <FlexContainer>
+          <TextContainer>
+            <SpaceBetweenContainer>
+              <Name>{recipeName}</Name>
+              <FlexContainer>
+                <Icon src={iconDelete} onClick={onClickDelete} />
               </FlexContainer>
-              <Contents>
-                {content}
-              </Contents>
-            </TextContainer>
-          </FlexContainer>
-        </CardContainer>
+            </SpaceBetweenContainer>
+            <Date>{lastModifiedDate.slice(0, 10)}</Date>
+            <FlexContainer style={{ justifyContent: "space-between" }}>
+              <Rating name="read-only" value={ratings} readOnly size="small" />
+            </FlexContainer>
+            <Contents>{content}</Contents>
+          </TextContainer>
+        </FlexContainer>
+      </CardContainer>
         <ImgWrapper>
-          <Img src={imgUrl} alt="이미지를 찾을 수 없습니다." />
+      <Link to={`/recipes/${recipe_seq}`} style={{ }}>
+          <Img src={recipeImg} alt="이미지를 찾을 수 없습니다." />
+      </Link>
         </ImgWrapper>
-      </Container>
+    </Container>
   );
 };
 
