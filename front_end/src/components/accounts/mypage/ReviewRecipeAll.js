4d6b2b2e5fc672c624020191432f6f90f8b756a8
runkey <csi9509@naver.com>
2022-03-29 04:01:18 +0900

    [S06P22A103-183][FE] Design: See All 디자인 변경
    Former-commit-id: 24809a66ff538b09fe223f02404d3faca767c1f7

--- front_end/src/components/accounts/mypage/ReviewRecipeAll.js
+++ front_end/src/components/accounts/mypage/ReviewRecipeAll.js
@@ -1,40 +1,40 @@
 import React, { useEffect, useState } from "react";
 import styled from "styled-components";
-import { userInfoState } from 'atoms/atoms';
-import { useRecoilValue } from 'recoil';
+import { userInfoState } from "atoms/atoms";
+import { useRecoilValue } from "recoil";
 
-import Card3 from "components/commons/Card3";
+import ReviewCard from "components/accounts/mypage/ReviewCard";
+
 import Tile from "assets/img/Tile.jpg";
 import "assets/css/Pagination.css";
 import Pagination from "react-js-pagination";
 import { getReview } from "api/MyPageApi";
 
-const Container = styled.div`
-`
+const Container = styled.div``;
 
 const Header = styled.div`
   height: 24rem;
   background-image: url(${Tile});
-`
+`;
 
 const HeaderContent = styled.div`
   position: absolute;
   top: 19rem;
-  left:50%;
-  transform: translate(-50%, -50%);                                                                   
+  left: 50%;
+  transform: translate(-50%, -50%);
   font-size: 3rem;
   font-style: italic;
   color: white;
   z-index: 2;
   text-align: center;
-`
+`;
 
 const BodyContainer = styled.div`
   display: flex;
   flex-direction: column;
   justify-content: center;
   padding: 4rem 16rem;
-`
+`;
 
 const Result = styled.div`
   display: flex;
@@ -45,43 +45,45 @@ const Result = styled.div`
     padding: 1rem;
   }
   .count {
-    color: #ED8141;
+    color: #ed8141;
     padding: 1rem;
   }
-`
+`;
 
 const CardContainer = styled.div`
   display: flex;
   flex-wrap: wrap;
   justify-content: center;
-`
+`;
 
 const PageContainer = styled.div`
   margin: 2rem 0 5rem 0;
-`
+`;
 
 const ReviewRecipeAll = () => {
   const UserInfo = useRecoilValue(userInfoState);
-  const [ count, setCount ] = useState(0)
-  const [ resultList, setResultList ] = useState([])
+  const [count, setCount] = useState(0);
+  const [resultList, setResultList] = useState([]);
+  const [page, setPage] = useState(1);
 
-  const [ page, setPage ] = useState(1);
-
-  const handlePageChange = async(page) => {
+  const handlePageChange = async (page) => {
     setPage(page);
-    getReview(page, UserInfo)
-  }
+    getReview(page, UserInfo);
+    const result = await getReview(page, UserInfo);
+    setResultList(result.review_list);
+  };
 
   useEffect(() => {
-    getReview(page, UserInfo).then((res) => {
-      console.log(res)
-      setCount(res.review_count)
-      setResultList(res.review_list)
-    })
-    .catch((err) => {
-      console.log(err)
-    })
-  },[])
+    getReview(page, UserInfo)
+      .then((res) => {
+        setCount(res.review_count);
+        setResultList(res.review_list);
+        console.log(res);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }, []);
 
   return (
     <Container>
@@ -93,32 +95,35 @@ const ReviewRecipeAll = () => {
           <div className="count">{count}</div>
         </Result>
         <CardContainer>
-          { resultList.map((result) => ( 
-            <Card3
-              key={result.recipe_seq}
-              {...result} 
+          {resultList.map((review) => (
+            <ReviewCard
+              key={review.id}
+              reviewId={review.id}
+              content={review.content}
+              ratings={review.ratings}
+              lastModifiedDate={review.last_modified_date}
+              recipe_seq={review.recipe_seq}
             />
           ))}
-
-          
-
         </CardContainer>
-        { resultList.length !== 0 ?
-          <PageContainer>
-            <Pagination 
-              activePage={page} 
-              itemsCountPerPage={10} 
-              totalItemsCount={resultList.length} 
-              pageRangeDisplayed={5} 
-              prevPageText={"‹"} 
-              nextPageText={"›"} 
-              onChange={handlePageChange}
-            />
-          </PageContainer> : null  // rendering 되기 전 pagination만 보이는 것 방지
+        {
+          resultList.length !== 0 ? (
+            <PageContainer>
+              <Pagination
+                activePage={page}
+                itemsCountPerPage={12}
+                totalItemsCount={count}
+                pageRangeDisplayed={5}
+                prevPageText={"‹"}
+                nextPageText={"›"}
+                onChange={handlePageChange}
+              />
+            </PageContainer>
+          ) : null // rendering 되기 전 pagination만 보이는 것 방지
         }
       </BodyContainer>
     </Container>
-  )
-}
+  );
+};
 
 export default ReviewRecipeAll;
