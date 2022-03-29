b1adf6b3d0829b7eb34187b2bc499cacf0779f30
runkey <csi9509@naver.com>
2022-03-29 17:55:07 +0900

    [S06P22A103-186][FE] Design: InfoModal 디자인
    Former-commit-id: f90dd9f0004af2c8b1d3ebd9f5512f33a514577e

--- front_end/src/components/accounts/modal/InfosModal.js
+++ front_end/src/components/accounts/modal/InfosModal.js
@@ -1,13 +1,11 @@
 import React from "react";
 import styled from "styled-components";
 import { motion } from "framer-motion";
-import Rosemary from "assets/img/Ingredient_rosemary.jpg"
-import Button from "../../commons/Button"
+import Button from "components/commons/Button";
 
 const Container = styled(motion.div)`
   box-sizing: border-box;
-  width: 1200px;
-  height: 700px;
+  width: 700px;
   border-radius: 40px;
   box-shadow: 0 2px 3px rgba(0, 0, 0, 0.1), 0 10px 20px rgba(0, 0, 0, 0.06);
   background-color: white;
@@ -16,138 +14,135 @@ const Container = styled(motion.div)`
   align-items: center;
   padding: 0 3rem;
   letter-spacing: -1px;
-`
+`;
 
 const Title = styled.div`
-  font-family: Playfair Display;
-  font-size: 40px;
+  display: flex;
+  justify-content: center;
+  font-size: ${(props) => (props.fs ? props.fs : "3rem")};
+  font-weight: ${(props) => (props.fw ? props.fw : "300")};
+  margin-top: ${(props) => (props.mt ? props.mt : "")};
+  margin-left: ${(props) => (props.ml ? props.ml : "")};
+  margin-bottom: ${(props) => (props.mb ? props.mb : "")};
+  margin-right: ${(props) => (props.mr ? props.mr : "")};
+`;
+
+const SpaceBetweenContainer = styled.div`
+  display: flex;
+  justify-content: space-around;
+`;
+
+const ButtonContainers = styled.div`
+  display: flex;
+  justify-content: center;
+`;
+
+const SelectContainer = styled.div`
+  display: flex;
+  justify-content: center;
   margin-top: 3rem;
-`
-
-const ItemContainer = styled.div`
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
-  align-items: center;
-  width: 1000px;
-`
-
-const Item = styled.div`
-  display: flex;
-  flex-direction: row;
-`
-
-const ImageContainer = styled.div`
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
-  align-items: center;
-  margin: 3rem;
-  margin-right: 7rem;
 `;
 
-const Image = styled.img`
-  border-radius: 4rem;
-  height: 8rem;
-  width: 8rem;
-  cursor: pointer;
+const SelectContent = styled.select`
+  width: 20rem;
+  height: 2rem;
+  font-size: 1rem;
+  font-family: Work Sans;
 `;
-
-const ImageSub = styled.div`
-  font-size: 15px;
-  margin-top: 1rem;
-  font-weight: bold;
-`
 
 const ButtonContainer = styled.div`
   display: flex;
+  margin: 3rem;
   margin-left: auto;
-  margin-right: 30px;
-`
+
+  .button {
+  }
+`;
 
 const InfosModal = ({ layoutId, setWidgetId }) => {
-    const onClick = (event) => {
-        event.stopPropagation();
-      };
+  const onClick = (event) => {
+    event.stopPropagation();
+  };
 
-      const onButton = () => {
-        setWidgetId(null);
-      }
+  const onButton = () => {
+    setWidgetId(null);
+  };
 
-    return (
-        <Container
-          onClick={onClick}
-          layout
-          layoutId={layoutId}
-          initial={{ opacity: 0 }}
-          animate={{ opacity: 1 }}
-          exit={{ opacity: 0 }}
-        >
-          <Title>Information</Title>
-          <ItemContainer>
-            <Item>
-              <ImageContainer>
-                <Image src={Rosemary}/>
-                <ImageSub>VEGETARIAN</ImageSub>
-              </ImageContainer>
-              
-              <ImageContainer>
-                <Image src={Rosemary}/>
-                <ImageSub>VEGETARIAN</ImageSub>
-              </ImageContainer>
-              
-              <ImageContainer>
-                <Image src={Rosemary}/>
-                <ImageSub>VEGETARIAN</ImageSub>
-              </ImageContainer>
 
-              <ImageContainer>
-                <Image src={Rosemary}/>
-                <ImageSub>VEGETARIAN</ImageSub>
-              </ImageContainer>
-            </Item>
-
-            <Item>
-              <ImageContainer>
-                <Image src={Rosemary}/>
-                <ImageSub>VEGETARIAN</ImageSub>
-              </ImageContainer>
-              
-              <ImageContainer>
-                <Image src={Rosemary}/>
-                <ImageSub>VEGETARIAN</ImageSub>
-              </ImageContainer>
-              
-              <ImageContainer>
-                <Image src={Rosemary}/>
-                <ImageSub>VEGETARIAN</ImageSub>
-              </ImageContainer>
-
-              <ImageContainer>
-                <Image src={Rosemary}/>
-                <ImageSub>VEGETARIAN</ImageSub>
-              </ImageContainer>
-            </Item>
-          </ItemContainer>
-
-          <ButtonContainer>
+  return (
+    <Container
+      onClick={onClick}
+      layout
+      layoutId={layoutId}
+      initial={{ opacity: 0 }}
+      animate={{ opacity: 1 }}
+      exit={{ opacity: 0 }}
+    >
+      <Title fs="2.5rem" fw="300" mt="2rem" mb="1rem">
+        Select your information
+      </Title>
+      <Title fs="1.2rem" fw="200" mb="2rem">
+        Check your gender and age.
+      </Title>
+      <ButtonContainers>
+        <div style={{ width: "26rem" }}>
+          <SpaceBetweenContainer>
             <Button
-            name = "확인"
-            onClick={onButton}
-            />
-
-            <Button
-              name = "취소"
-              bc="#C4C4C4"
+              mt="1rem"
+              w="12rem"
+              h="5rem"
+              br="1rem"
+              bc="white"
+              bs="1px 1px 10px 3px #e2e2e2"
+              border="1px solid grey"
+              fontsize="1rem"
               hoverColor="#a2a2a2"
-              ml = "3rem"
-              onClick={onButton}
+              name="Man"
             />
-          </ButtonContainer>
+            <Button
+              mt="1rem"
+              w="12rem"
+              h="5rem"
+              br="1rem"
+              bc="white"
+              bs="1px 1px 10px 3px #e2e2e2"
+              border="1px solid grey"
+              fontsize="1rem"
+              hoverColor="#a2a2a2"
+              name="Woman"
+            />
+          </SpaceBetweenContainer>
+        </div>
+      </ButtonContainers>
+      <SelectContainer>
+        <Title mt="0.5rem" fs="1rem" mr="0.5rem">
+          AGE
+        </Title>
+        <SelectContent>
+          <option value="0" selected disabled>
+            select age
+          </option>
+          <option value="1">15-19 years old</option>
+          <option value="2">20-29 years old</option>
+          <option value="3">30-49 years old</option>
+          <option value="4">50-64 years old</option>
+          <option value="5">65-74 years old</option>
+          <option value="6">75 years of age or older</option>
+        </SelectContent>
+      </SelectContainer>
+      <ButtonContainer>
+        <Button name="Check" onClick={onButton} />
 
-        </Container>
-      );
-    }
-  
-  export default InfosModal;
-  
+        <Button
+          name="Cancel"
+          bc="#C4C4C4"
+          hoverColor="#a2a2a2"
+          ml="3rem"
+          onClick={onButton}
+        />
+      </ButtonContainer>
+    </Container>
+  );
+};
+
+export default InfosModal;
